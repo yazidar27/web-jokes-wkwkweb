ca5acc1a63f7c5022eb30dacb43f289c1cff3550
Aqsa Raisnaldi <yazidar27@gmail.com>
2025-03-05 19:18:28 +0700

    change font from hero-section

--- frontend/src/components/buttons/ButtonLink.jsx
+++ frontend/src/components/buttons/ButtonLink.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ButtonLink = ({ onClick, title, to, ...props }) => {
-    return (
-        <Link
-            className="text-white bg-pink-500 hover:bg-pink-600
+  return (
+    <Link
+      className="text-white bg-pink-500 hover:bg-pink-800
             focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium
             rounded-lg text-sm px-4 py-2 text-center w-full"
-            to={to}
-            {...props}
-        >
-            {title}
-        </Link>
-    )
-}
+      to={to}
+      {...props}
+    >
+      {title}
+    </Link>
+  );
+};
 
-export default ButtonLink
+export default ButtonLink;
--- frontend/src/pages/IntroPage.jsx
+++ frontend/src/pages/IntroPage.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import TypeWriter from '../components/TypeWriter';
-import ButtonLink from '../components/buttons/ButtonLink';
+import React from "react";
+import TypeWriter from "../components/TypeWriter";
+import ButtonLink from "../components/buttons/ButtonLink";
 
 const IntroPage = () => {
-    return (
-        <div className='flex flex-col min-h-[50vh] justify-center items-center'>
+  return (
+    <div className="flex flex-col min-h-[70vh] justify-center items-center">
+      <TypeWriter
+        className="text-gray-800 text-center font-semibold text-4xl p-4 font-display tracking-wide mb-5"
+        text="Write your best jokes and make people laugh."
+      />
+      <p className="text-center text-lg text-gray-700">
+        Everyone has a sense of humor
+      </p>
 
-            <TypeWriter
-                className="text-gray-800 text-center font-semibold text-4xl p-4 font-display tracking-wide mb-5"
-                text="Write your best jokes and make people laugh."
-            />
-            <p className="text-center text-lg text-gray-700">
-                Everyone has a sense of humor
-            </p>
-
-            <div className='flex flex-row gap-4 mt-4'>
-                <ButtonLink title="Start Writing Jokes" to="jokes/create" />
-                <ButtonLink title="Explore Jokes" />
-            </div>
-        </div>
-    )
-}
+      <div className="flex flex-row justify-center align-center p-3 gap-4 mt-8">
+        <ButtonLink title="Start Writing Jokes" to="jokes/create" />
+        <ButtonLink title="Explore Jokes" />
+      </div>
+    </div>
+  );
+};
 
 export default IntroPage;
